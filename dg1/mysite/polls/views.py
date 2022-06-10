4f4862d29f107895046e0167a8f89439f94c7f0b
Rajan-drc <rajan.rajgor@drcsystems.com>
2022-06-10 10:48:57 +0530

    simple views created succesfully

--- dg1/mysite/polls/views.py
+++ dg1/mysite/polls/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
 
+
 def index(request):
 	return HttpResponse("Hello Rajan good work now you are about to use DJANGO and GIT to gather!")
+
+
+def detail(request, question_id):
+	return HttpResponse("Detail: question_id %s" % question_id)
+
+
+def results(request, question_id):
+	response = "results: You're looking at the result the results of question %s"
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("vote: question_id %s" % question_id)
